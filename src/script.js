0900aa22abb55e2c07437d0267f5a043922bad29
Quentin Goujon <quentin.goujon@epita.fr>
2023-06-07 11:23:40 +0200

    yolo

--- src/script.js
+++ src/script.js
@@ -7,18 +7,44 @@ translateButton.addEventListener('click', () => {
     const input = inputText.value;
     const language = languageSelect.value;
 
-    // Ici, vous pouvez utiliser une API de traduction ou ajouter votre propre logique de traduction en JavaScript
+    const key = "294d8833e2684e3dacc9c0b9c68285f7";
+    const location = "francecentral";
+    const endpoint = "https://api.cognitive.microsofttranslator.com";
 
-    // Exemple simple de traduction avec un objet de traduction
-    const translations = {
-        en: 'Hello',
-        fr: 'Bonjour',
-        es: 'Hola'
+    const path = '/translate';
+    const constructed_url = endpoint + path;
+
+    const params = {
+        'api-version': '3.0',
+        'from': 'fr',
+        'to': [language]
     };
 
-    if (translations.hasOwnProperty(language)) {
-        outputText.textContent = translations[language] + ': ' + input;
-    } else {
-        outputText.textContent = 'Langue non prise en charge';
-    }
+    const headers = {
+        'Ocp-Apim-Subscription-Key': key,
+        'Ocp-Apim-Subscription-Region': location,
+        'Content-type': 'application/json',
+    };
+
+    const body = [{
+        'text': input
+    }];
+
+    fetch(constructed_url + '?' + new URLSearchParams(params), {
+        method: 'POST',
+        headers: headers,
+        body: JSON.stringify(body)
+    })
+        .then(response => response.json())
+        .then(data => {
+            if (data[0] && data[0].translations && data[0].translations[0] && data[0].translations[0].text) {
+                outputText.textContent = data[0].translations[0].text;
+            } else {
+                outputText.textContent = 'Erreur de traduction';
+            }
+        })
+        .catch(error => {
+            console.error('Une erreur s\'est produite:', error);
+            outputText.textContent = 'Erreur de traduction';
+        });
 });
